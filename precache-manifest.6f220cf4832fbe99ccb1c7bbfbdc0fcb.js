68b4ea42cd75f50245bd8549952dadce8141952a
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-09-12 14:17:25 -0700

    Updates

---
+++ precache-manifest.6f220cf4832fbe99ccb1c7bbfbdc0fcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b88aa08bdaa25a25696d418f1f989d3",
+    "url": "/github-API/index.html"
+  },
+  {
+    "revision": "98ea875648d0c33233f8",
+    "url": "/github-API/static/css/main.15851efa.chunk.css"
+  },
+  {
+    "revision": "60eab539c66b10f5114a",
+    "url": "/github-API/static/js/2.7eacb1db.chunk.js"
+  },
+  {
+    "revision": "98ea875648d0c33233f8",
+    "url": "/github-API/static/js/main.663650fd.chunk.js"
+  },
+  {
+    "revision": "7554e3dd7552d14b963d",
+    "url": "/github-API/static/js/runtime~main.bc2c8c25.js"
+  }
+]);
